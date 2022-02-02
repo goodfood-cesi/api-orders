4b804e85ea4c75cccb5d7115d48701fc50323f96
Flavien Chagras <flavien.chagras@protonmail.com>
2022-02-02 23:05:16 +0100

    feat(app): migration orders created

---
+++ database/migrations/2022_02_02_220237_create_orders_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersRowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('order_id');
+            $table->unsignedInteger('product_id');
+            $table->float('amount_untaxed');
+            $table->unsignedInteger('quantity');
+            $table->timestamps();
+            $table->foreign('order_id')->references('id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders_products');
+    }
+}
