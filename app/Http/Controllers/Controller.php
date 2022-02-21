62e2bf2c58d3ae527c5df5b29fab41c06f2bd929
Flavien Chagras <flavien.chagras@protonmail.com>
2022-02-21 20:42:35 +0100

    feat(app): add fetch method to controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponser;
+use Exception;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
-class Controller extends BaseController
-{
-    //
+class Controller extends BaseController{
+    use ApiResponser;
+
+    /**
+     * @throws \JsonException
+     * @throws Exception
+     */
+    protected function fetch(string $endpoint) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => getenv('RESTAURANTS_API_URL') . $endpoint,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        if(!$response) {
+            throw new Exception('Can\'t connect to RESTAURANT API');
+        }
+        return json_decode($response, true, 512, JSON_THROW_ON_ERROR);
+    }
 }
