383b723fe61eaeba3f6cfd378d2607b925e0836e
Flavien Chagras <flavien.chagras@protonmail.com>
2022-02-21 20:33:55 +0100

    feat(app): migrations added

--- database/migrations/2022_02_02_220237_create_orders_products_table.php
+++ database/migrations/2022_02_02_220237_create_orders_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersRowsTable extends Migration
+class CreateOrdersProductsTable extends Migration
 {
     /**
      * Run the migrations.
