41fc2e630d711858e889c63fb889f1004ba76f25
Flavien Chagras <flavien.chagras@protonmail.com>
2022-07-18 21:40:02 +0200

    tests(app): @covers added

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -9,10 +9,19 @@ class OrderTest extends TestCase
 {
     use DatabaseMigrations;
 
+    /**
+     * @covers App\Http\Middleware\Authenticate
+     * @covers App\Http\Controllers\OrderController
+     * @covers App\Http\Resources\OrderResource
+     * @covers App\Models\Order
+     * @covers App\Models\User
+     * @covers App\Traits\ApiResponser
+     * @return void
+     */
     public function test_can_get_all_orders(): void
     {
         $user = User::factory()->create();
-        $order = Order::factory()->count(50)->create([
+        Order::factory()->count(50)->create([
             'user_id' => $user->id
         ]);
 
@@ -53,6 +62,15 @@ class OrderTest extends TestCase
         ]);
     }
 
+    /**
+     * @covers App\Http\Middleware\Authenticate
+     * @covers App\Http\Controllers\OrderController
+     * @covers App\Http\Resources\OrderResource
+     * @covers App\Models\Order
+     * @covers App\Models\User
+     * @covers App\Traits\ApiResponser
+     * @return void
+     */
     public function test_can_get_one_order(): void
     {
         $user = User::factory()->create();
@@ -91,6 +109,33 @@ class OrderTest extends TestCase
             'meta' => [
                 'success',
                 'message'
+            ]
+        ]);
+    }
+
+    /**
+     * @covers App\Models\Order
+     * @covers App\Models\User
+     * @covers App\Http\Controllers\OrderController
+     * @covers App\Http\Middleware\Authenticate
+     * @covers App\Traits\ApiResponser
+     * @return void
+     */
+    public function test_cannot_get_one_order(): void {
+        $user = User::factory()->create();
+        $order = Order::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        $userTest = User::factory()->create();
+
+        $this->get(route('orders.show', ['id' => $order->id]), ['Authorization' => 'Bearer ' . JWTAuth::fromUser($userTest)]);
+        $this->seeStatusCode(404);
+        $this->seeJson([
+            'data' => 404,
+            'meta' => [
+                'success' => false,
+                'message' => 'Order does not exist.',
             ]
         ]);
     }
